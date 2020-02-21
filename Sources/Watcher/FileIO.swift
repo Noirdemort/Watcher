9d0b1764a8b5fd26a7f0886959f916eb4b126459
noirdemort <darkStillwater@protonmail.com>
2020-02-21 12:09:24 +0530

    add File and JSON operations

---
+++ Sources/Watcher/FileIO.swift
@@ -0,0 +1,87 @@
+//
+//  FileIO.swift
+//  Watcher
+//
+//  Created by Noirdemort on 21/02/20.
+//
+
+import Foundation
+import CryptoSwift
+
+
+protocol JSONOperator {
+    func readJSON(encrypted: Bool) throws -> [Account]
+    mutating func updateCore(_ account: Account)
+    func saveJSON(encrypt: Bool) throws
+}
+
+
+struct JSOC: JSONOperator {
+  
+    static var jdam = JSOC()
+    
+    let fileManager = FileManager()
+    let file = "blackburn.json"
+    
+    var allAccounts: [Account] = []
+    
+    
+    private init() {
+        allAccounts = try! readJSON()
+    }
+    
+    
+    func readJSON(encrypted: Bool = false) throws -> [Account] {
+        
+        guard let dir = self.fileManager.urls(for: .documentDirectory, in: .userDomainMask).first else {
+            print("[!] Can't retrieve the resource")
+            exit(EXIT_FAILURE)
+        }
+        
+        let fileURL = dir.appendingPathComponent(self.file)
+        
+        var data = try Data(contentsOf: fileURL)
+        
+        if encrypted {
+            let bytes = [UInt8](data)
+            let secureKey = consoleInput(annotation: "Enter decryption key: ", required: true, secureInput: true)
+            let iv = consoleInput(annotation: "Enter decryption IV: ", required: true, secureInput: true)
+            let aes = try AES(key: secureKey, iv: iv)
+            data = try aes.decrypt(bytes).data
+        }
+        
+        let decoder = JSONDecoder()
+        
+        return try decoder.decode([Account].self, from: data)
+        
+    }
+    
+    mutating func updateCore(_ account: Account) {
+        self.allAccounts.removeAll(where: { $0.username == account.username })
+        self.allAccounts.append(account)
+    }
+    
+    
+    func saveJSON(encrypt: Bool = false) throws {
+        let encoder = JSONEncoder()
+        var data = try encoder.encode(self.allAccounts)
+        
+        guard let dir = fileManager.urls(for: .documentDirectory, in: .userDomainMask).first else {
+            print("[!] Can't retrieve the resource")
+            exit(EXIT_FAILURE)
+        }
+        
+        let fileURL = dir.appendingPathComponent(self.file)
+        
+        if encrypt {
+            let bytes = [UInt8](data)
+            let secureKey = consoleInput(annotation: "Enter encryption key: ", required: true, secureInput: true)
+            let iv = consoleInput(annotation: "Enter encryption IV: ", required: true, secureInput: true)
+            let aes = try AES(key: secureKey, iv: iv)
+            data = try aes.encrypt(bytes).data
+        }
+        
+        try data.write(to: fileURL)
+    }
+    
+}
